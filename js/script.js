d50518e525d3383aef65a1b3fbd7cfec4bdf7c62
Osaym <68712890+Osaym@users.noreply.github.com>
2022-12-29 17:38:46 -0500

    Update script.js
    I felt like putting a comment idk

--- js/script.js
+++ js/script.js
@@ -1,2 +1,3 @@
+// Simple JavaScript to display the current year in the footer
 const currentYear = new Date().getFullYear();
 document.getElementById("footer").innerHTML = "&copy; " + currentYear + " Osaym Omar";
