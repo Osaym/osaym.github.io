52343125405385d3b936766643051e1b062d368e
Osaym <68712890+Osaym@users.noreply.github.com>
2022-12-30 21:22:39 -0500

    Added font changing feature
    buttons to change font has been added

--- js/clockScript.js
+++ js/clockScript.js
@@ -1,3 +1,4 @@
+// Function to update the clock
 function updateTime() {
     var date = new Date();
     var hours = date.getHours();
@@ -13,6 +14,29 @@ function updateTime() {
     document.getElementById("hours").innerHTML = hours;
     document.getElementById("minutes").innerHTML = minutes;
     document.getElementById("seconds").innerHTML = seconds;
-  }
-  // Update the time every 1000 milliseconds (1 second)
-  setInterval(updateTime, 1000);
+}
+
+function changeFont1() {
+  // Get the text element and the button
+  var text = document.getElementById("time");
+  var button = document.getElementById("font1");
+
+  text.style.fontFamily = "Helvetica";
+  button.innerHTML = "Change font again to Helvetica";
+}
+
+function changeFont2() {
+  // Get the text element and the button
+  var text = document.getElementById("time");
+  var button = document.getElementById("font2");
+
+  text.style.fontFamily = 'Anton';
+  button.innerHTML = "Change font again to Anton";
+}
+
+// Update the time every 1000 milliseconds (1 second)
+setInterval(updateTime, 1000);
+
+// changeFont1 and changeFont2 are called when the buttons are clicked
+document.getElementById("font1").addEventListener("click", changeFont1);
+document.getElementById("font2").addEventListener("click", changeFont2);
